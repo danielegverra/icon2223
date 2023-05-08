935b817e3256cbafd5c3dc3e36cd82606ea584a4
NicolaLassandro <n.lassandro4@studenti.uniba.it>
2023-05-08 18:01:36 +0200

    modified api call (now google)

--- Preprocessor.py
+++ Preprocessor.py
@@ -1,26 +1,33 @@
+import logging as log
 import requests as rq
+import time as tm
+
 
 from Landmark import Landmark
+from yelpapi import YelpAPI as yelp
 
 
 def apiCall(latitude: float, longitude: float):
-    return rq.get(
-        f"https://api.opentripmap.com/0.1/en/places/radius?radius=1500&lon={longitude}&lat={latitude}&rate=3&format=json&limit=1000&apikey=5ae2e3f221c38a28845f05b6c4fa232e8d7430baddba0e7f4d0e6941"
-    )
+    api_key = "AIzaSyDLviZEMhmqbosipmU7_LMTJkFtKeeeMJA"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={latitude},{longitude}&type=tourist_attraction&rankby=distance&key={api_key}"
+    return rq.get(url)
 
 
-def responseInMap(map: dict, resp: rq.Response):
-    data = resp.json()
-    for element in data:
-        poi = Landmark(
-            element["name"],
-            None,
-            element["wikidata"],
-            element["kinds"],
-            element["point"]["lat"],
-            element["point"]["lon"],
-        )
-        poiMap[poi.refWiki] = poi
+def apiCallNext(pagetoken: str):
+    api_key = "AIzaSyDLviZEMhmqbosipmU7_LMTJkFtKeeeMJA"
+    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?pagetoken={pagetoken}&key={api_key}"
+    return rq.get(url)
+
+
+def extractResults(list: list, response: rq.Response):
+    list += response.json().get("results")
+    page_token = response.json().get("next_page_token")
+    while page_token:
+        tm.sleep(2)
+        response = apiCallNext(page_token)
+        list += response.json().get("results")
+        page_token = response.json().get("next_page_token")
+    return list
 
 
 def printMap(map: dict):
@@ -32,32 +39,53 @@ def printMap(map: dict):
 
 
 def printMapLength(map: dict):
-    print(poiMap.__len__())
+    print(map.__len__())
 
+
+# START APPLICATION
+
+# Log configuration
+log.basicConfig(level=log.INFO)
+handler = log.FileHandler("log.txt", mode="w")
+handler.setLevel(log.INFO)
+log.getLogger("").addHandler(handler)
 
 # Coordinates of the "tourist" center of Rome
 centerLatitude = 41.8995134
 centerLongitude = 12.4762505
 
-# Coordinate offset
+# Coordinate offset 1.5km
 latOffset = 0.0135
 lonOffset = 0.0176
 
 # Multiple calls due to api limitations: Est, West, North, South
+data = list()
 responseEst = apiCall(centerLatitude, centerLongitude + lonOffset)
+data = extractResults(data, responseEst)
+"""
 responseWest = apiCall(centerLatitude, centerLongitude - lonOffset)
 responseNorth = apiCall(centerLatitude + latOffset, centerLongitude)
 responseSouth = apiCall(centerLatitude - latOffset, centerLongitude)
+"""
+
+for i in data:
+    print(i["name"])
+    print(i["geometry"]["location"])
+    print(i["types"])
+    print(i["vicinity"])
+    print()
+print(data.__len__())
 
 
+"""
 if (
     (responseEst.status_code == 200)
     & (responseWest.status_code == 200)
     & (responseNorth.status_code == 200)
     & (responseSouth.status_code == 200)
 ):
-    print("Data obtained correctly.")
-
+    log.info("Data obtained correctly.")
+    
     poiMap = {}
     responseInMap(poiMap, responseEst)
     responseInMap(poiMap, responseWest)
@@ -68,4 +96,5 @@ if (
     # printMap(poiMap)
 
 else:
-    print("Error in obtaining data.")
+    log.info("Error in obtaining data.")
+"""
